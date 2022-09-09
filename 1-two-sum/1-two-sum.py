e992ac6fc301195278ddaa939629bdda7df025cd
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2022-09-09 10:46:22 +0530

    Time: 137 ms (40.15%), Space: 15.2 MB (23.99%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Solution is O(n)
+        # Note that there is only 1 index pair which satisfies
+        d = {} # {element:index}
+        for i in range(len(nums)):
+            a = nums[i]
+            b = target - nums[i]
+            if b in d:
+                # Return statement occurs before appending to dictionary. Reason: Dry run nums=[3, 3] and target=6
+                return [d[b], i] # return [index of b, i]
+            # Add current element, index to dictionary
+            d[a] = i
