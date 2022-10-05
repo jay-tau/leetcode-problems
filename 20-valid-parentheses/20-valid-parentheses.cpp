ce2c09c4d495dec01d347f0a35557d9ff64b0078
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2022-10-05 17:15:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.48%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s[i];
+
+            if ((c == '(') || (c == '[') || (c == '{'))
+                st.push(c);
+            else if (!st.empty()) {
+                char top = st.top();
+                if (((c == ')') && (top == '(')) || ((c == ']') && (top == '[')) || ((c == '}') && (top == '{')))
+                    st.pop();
+                else
+                    return false;
+            } else
+                return false;
+        }
+
+        return (st.empty());
+    }
+};
