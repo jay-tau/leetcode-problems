801d232aa1c967dfd9d60dc10967e2531e4d94be
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2022-11-07 21:10:29 +0530

    Time: 7 ms, Memory: 8.9 MB - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // 0 <= nums[i] <= 50
+        int ans = nums.size();
+        for (int i =0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                nums[i] = 1000;
+                ans--;
+            }
+        }
+        sort(nums.begin(), nums.end());
+        return ans;
+    }
+};
