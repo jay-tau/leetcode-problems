065ac96965b6774ef9739e241d6a53383ad456bc
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2023-04-12 23:52:42 +0530

    Time: 44 ms (70.12%), Space: 18.4 MB (56.80%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -16,15 +16,11 @@ public:
     vector<vector<bool>> visited_atlantic(num_rows, vector<bool>(num_cols, 0));
 
     for (int i = 0; i < num_rows; ++i) { // Vertical border
-      // visited_pacific[i][0] = -1;
       to_visit_pacific.push({i, 0});
-      // visited_atlantic[i][num_cols - 1] = -1;
       to_visit_atlantic.push({i, num_cols - 1});
     }
     for (int i = 0; i < num_cols; ++i) { // Horizontal border
-      // visited_pacific[0][i] = -1;
       to_visit_pacific.push({0, i});
-      // visited_atlantic[num_rows - 1][i] = -1;
       to_visit_atlantic.push({num_rows - 1, i});
     }
 
@@ -33,7 +29,6 @@ public:
       pair<int, int> current_cell = to_visit_pacific.front();
       to_visit_pacific.pop();
       int x = current_cell.first, y = current_cell.second;
-
       int current_height = heights[x][y];
 
       if (visited_pacific[x][y])
@@ -81,17 +76,6 @@ public:
         to_visit_atlantic.push({x, y + 1});
     }
 
-    // for (auto v : visited_pacific) {
-    //   for (auto x : v)
-    //     cout << x << " ";
-    //   cout << endl;
-    // }
-    // cout << endl;
-    // for (auto v : visited_atlantic) {
-    //   for (auto x : v)
-    //     cout << x << " ";
-    //   cout << endl;
-    // }
     vector<vector<int>> result;
     for (int i = 0; i < num_rows; ++i) {
       for (int j = 0; j < num_cols; ++j) {
