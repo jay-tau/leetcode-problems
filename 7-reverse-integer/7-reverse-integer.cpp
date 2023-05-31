0affcd19f0ada308804bc6372e681e3eb95f9e56
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2023-05-31 20:08:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.81%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,19 +1,17 @@
 class Solution {
-public:
-    int reverse(int x) {
-            int x_rev = 0, x_copy = abs(x);
-        
-        while (x_copy > 0) {
-            if (x_rev > (INT_MAX/10))
-                return 0;
-            x_rev *= 10;
-            x_rev += (x_copy%10);
-            x_copy /= 10;
-        }
-        
-        if (x < 0)
-            x_rev *= -1;
-        
-        return x_rev;
+ public:
+  int reverse(int x) {
+    int x_rev = 0, x_copy = abs(x);
+
+    while (x_copy > 0) {
+      if (x_rev > (INT_MAX / 10)) return 0;
+      x_rev *= 10;
+      x_rev += (x_copy % 10);
+      x_copy /= 10;
     }
+
+    if (x < 0) x_rev *= -1;
+
+    return x_rev;
+  }
 };
