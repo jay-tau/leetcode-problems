5c0af39b402f1844cafcfed1aa8030600f310fd5
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2023-06-01 12:44:27 +0530

    Time: 9 ms (20.18%), Space: 8.8 MB (64.43%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -6,23 +6,19 @@ class Solution {
     if (n == 1) return 0;
     if (n == 2) return ((nums[0] > nums[1]) ? 0 : 1); // Return largest of 2 elements
 
-    int l = -1, r = n;
+    int l = 0, r = n-1;
     while (l < r) {
       int mid = l + (r - l) / 2;
+      
       int current_element = nums[mid];
       int left_element = ((mid - 1) <= -1) ? INT_MIN : nums[mid - 1];
       int right_element = ((mid + 1) >= n) ? INT_MIN : nums[mid + 1];
       
-      if ((left_element < current_element) && (current_element > right_element)) {
-        cout << "exit inside" << endl;
-        return mid;
-      }
-      
-      if ((left_element > current_element) && (current_element > right_element)) // Decreasing
+      if (current_element > right_element) // Decreasing
         r = mid; // Move left
       else
         l = mid + 1; // Move right
     }
-    return (l-1);
+    return l;
   }
 };
