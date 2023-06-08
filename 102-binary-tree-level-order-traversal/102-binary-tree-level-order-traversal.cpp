c2a6c23f70949763cc8e5e3172c134025c4ce676
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2023-06-08 14:40:14 +0530

    Time: 11 ms (15.20%), Space: 13.7 MB (8.58%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -6,37 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if (root == nullptr)
-            return {};
-        
-        vector<vector<int>> level_order_traversal;
-        queue<TreeNode*> q;
-        
-        q.push(root);
-        
-        while (!q.empty()) {
-            int level_size = q.size();
-            vector<int> level;
-            while (level_size--) {
-                TreeNode* current_node = q.front();
-                q.pop();
-                
-                if (current_node == nullptr)
-                    continue;
-                
-                level.push_back(current_node->val);
-                q.push(current_node->left);
-                q.push(current_node->right);
-            }
-            if (level.size() > 0)
-                level_order_traversal.push_back(level);
-        }
-        return level_order_traversal;
+ public:
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    if (root == nullptr) return {};
+
+    vector<vector<int>> level_order_traversal;
+    queue<TreeNode*> q;
+
+    q.push(root);
+
+    while (!q.empty()) {
+      int level_size = q.size();
+      vector<int> level;
+
+      while (level_size--) {
+        TreeNode* current_node = q.front();
+        q.pop();
+
+        if (current_node == nullptr) continue;
+
+        level.push_back(current_node->val);
+        q.push(current_node->left);
+        q.push(current_node->right);
+      }
+      if (level.size() > 0) level_order_traversal.push_back(level);
     }
+    return level_order_traversal;
+  }
 };
