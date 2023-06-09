68de59d8b000c09cb73e18bb995e707881bcf006
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2023-06-09 20:45:16 +0530

    Time: 16 ms (34.80%), Space: 20.3 MB (48.43%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -6,25 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-private:
-    int max_diameter = 0;
-    int height(TreeNode* node) {
-        if (node == nullptr)
-            return 0;
-        
-        int left_height = height(node->left), right_height = height(node->right);
-        
-        max_diameter = max(max_diameter, (left_height + right_height)); // Used to update global max diameter value. Only line that differs from a height function
-        
-        return 1 + max(left_height, right_height); // Returns height
-    }
-public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        height(root);
-        return max_diameter;
-    }
+ private:
+  int max_diameter = 0;
+  int height(TreeNode* node) {
+    if (node == nullptr) return 0;
+
+    int left_height = height(node->left), right_height = height(node->right);
+
+    max_diameter = max(max_diameter,
+                       (left_height + right_height));  // Used to update global
+    // max diameter value.
+    // Only line that differs
+    // from a height function
+
+    return 1 + max(left_height, right_height);  // Returns height
+  }
+
+ public:
+  int diameterOfBinaryTree(TreeNode* root) {
+    height(root);
+    return max_diameter;
+  }
 };
