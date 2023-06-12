8fefdaa0b1527dcb8630c0fdf53333d10ffea47b
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2023-06-12 21:18:31 +0530

    Time: 9 ms (23.33%), Space: 12.6 MB (74.21%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -30,8 +30,7 @@ public:
             if (current_node == nullptr)
                 continue;
             
-            // Insert into its position
-            m[current_offset][current_level].push_back(current_node->val);
+            m[current_offset][current_level].push_back(current_node->val); // Insert into its position
             
             // Add children into queue
             if (current_node->left != nullptr)
@@ -40,11 +39,16 @@ public:
                 q.push({current_node->right, {current_offset+1, current_level+1}});
         } // End while
         
-        for (pair<int, map<int, vector<int>>> p1: m) {
-            vector<int> vertical_traversal_offset_temp;
-            for (pair<int, vector<int>> p2: p1.second) {
-                sort(p2.second.begin(), p2.second.end());
-                for (int x: p2.second)
+        for (pair<int, map<int, vector<int>>> outer_map: m) {
+            
+            map<int, vector<int>> inner_map = outer_map.second;
+            
+            vector<int> vertical_traversal_offset_temp; // All offsets need to be in a single vector
+            
+            for (pair<int, vector<int>> p2: inner_map) {
+                vector<int> level_in_offset = p2.second;
+                sort(level_in_offset.begin(), level_in_offset.end()); // Sorting only occurs within a level
+                for (int x: level_in_offset)
                     vertical_traversal_offset_temp.push_back(x);
             }
             vertical_traversal.push_back(vertical_traversal_offset_temp);
