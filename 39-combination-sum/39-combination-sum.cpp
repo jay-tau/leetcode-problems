3e3a96ab3adc3f39921698b8e24f1a7eb6da9e08
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2023-07-05 03:16:09 -0700

    Time: 0 ms (100.00%), Space: 10.7 MB (66.41%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,31 +1,30 @@
 class Solution {
 private:
-    void f(int index, vector<int> &v, int target, vector<vector<int>> &ans, vector<int> &arr) {
-        if (target == 0) {
-            ans.push_back(v); // Target reached
+    void f(int index, int target, vector<int> &v, vector<int> &arr, vector<vector<int>> &ans) {
+        int n = arr.size();
+        if ((index > n) || (target < 0)) { // Impossible
+            cout << "ERROR" << endl;
             return;
         }
-        if (target < arr[0])
-            return; // Dead end
+        if (index == n) {
+            if (target == 0)
+                ans.push_back(v);
+            return;
+        }
         
-        for (int i = index; i >= 0; --i) {
-            if (arr[i] > target)
-                continue;
-            
-            v.push_back(arr[i]);
-            f(i, v, (target-arr[i]), ans, arr);
+        if (arr[index] <= target) {
+            v.push_back(arr[index]);
+            f(index, target-arr[index], v, arr, ans); // Same index can be taken again
             v.pop_back();
         }
+        f(index+1, target, v, arr, ans); // Do not take. Skip to next element
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n = candidates.size();
-        sort(candidates.begin(), candidates.end());
-        
         vector<vector<int>> ans;
         vector<int> v;
         
-        f(n-1, v, target, ans, candidates);
+        f(0, target, v, candidates, ans);
         
         return ans;
     }
