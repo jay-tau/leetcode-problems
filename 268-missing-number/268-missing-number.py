03859c3453575f693c3d01cc060b59fe3424e9ad
Joel Tony <joeltony72@gmail.com>
2024-01-20 23:06:19 +0530

    Clean up repo

--- 268-missing-number/268-missing-number.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
-        expected_sum = (n*(n+1))//2
-        return (expected_sum - sum(nums))
