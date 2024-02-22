9751cbc8f1b589fbdd2cda0a1b922000f1c9f000
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2024-02-22 18:52:15 +0530

    Time: 2 ms (46.74%), Space: 7.4 MB (6.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,19 @@
 class Solution {
-private:
-    vector<int> memo;
-    int dp(int i, const int n) {
-        if (i > n) return 0;
-        
-        if (i == n) return 1;
-        
-        if (memo[i] != -1) return memo[i];
-        
-        return memo[i] = dp(i+1, n) + dp(i+2, n);
-    }
-public:
-    int climbStairs(int n) {
-        memo.resize(n, -1);
-        return dp(0, n);
-    }
+ private:
+  vector<int> memo;
+  int dp(int i, const int n) {
+    if (i > n) return 0;
+
+    if (i == n) return 1; // Valid solution
+
+    if (memo[i] != -1) return memo[i];
+
+    return memo[i] = dp(i + 1, n) + dp(i + 2, n);
+  }
+
+ public:
+  int climbStairs(int n) {
+    memo.resize(n, -1);
+    return dp(0, n);
+  }
 };
