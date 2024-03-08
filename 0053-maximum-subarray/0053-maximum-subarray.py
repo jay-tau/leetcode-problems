43924e4cd8475e6222b807cd051bae7c063eeeb2
Joel Tony <95355656+jay-tau@users.noreply.github.com>
2024-03-08 12:34:31 +0530

    Time: 515 ms (77.20%), Space: 30.8 MB (87.15%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        current_sum = 0
+        for num in nums:
+            if current_sum < 0: # Do not carry -ve sums forward
+                current_sum = 0 # Reset the front end of the sliding window
+            current_sum += num
+            max_sum = max(max_sum, current_sum)
+        return max_sum
