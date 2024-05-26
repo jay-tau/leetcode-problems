cae18f40dbe3457793004ab3e857b3894a01f440
Joel Tony <joeltony72@gmail.com>
2024-05-26 15:12:55 +0530

    Time: 45 ms (6.54%), Space: 16.4 MB (93.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,9 +1,3 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        pascal_triangle = [[1]]
-        
-        for n in range(1, numRows):
-            row = [comb(n, r) for r in range(n+1)]
-            pascal_triangle.append(row)
-    
-        return pascal_triangle
+        return [[comb(n, r) for r in range(n+1)] for n in range(0, numRows)]
