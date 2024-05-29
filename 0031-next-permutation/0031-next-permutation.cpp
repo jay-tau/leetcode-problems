bd01ac7ffb1fb6ac0531de688f4fa986213d0942
Joel Tony <joeltony72@gmail.com>
2024-05-29 22:01:45 +0530

    Time: 8 ms (14.56%), Space: 14.5 MB (53.69%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        // From the back, move pointer till we find a drop, i.e. a[i] < a[i+1]. If no drop is found, then sort array and return
-        int n = nums.size();
-        int i_break = -1;
-        for (int i = 0; i < (n-1); i++)
-            if (nums[i] < nums[i+1])
-                i_break = i;
-        
-        // cout << i_break << endl;
-        
-        if (i_break == -1) { // Fully descending
-            sort(nums.begin(), nums.end());
-            return;
-        }
-        
-        // For the remaining elements, find x (smallest element greater than a[i])
-        // auto x_pos = upper_bound(nums.begin() + (i_break+1), nums.end(), nums[i_break]);
-        int x = INT_MAX;
-        int x_pos = -1;
-        for (int i = i_break + 1; i < n; i++)
-            if ((nums[i] > nums[i_break]) && (nums[i] < x)) {
-                x = nums[i];
-                x_pos = i;
-            }
-        // Swap nums[i] with x
-        swap(nums[i_break], nums[x_pos]);
-        
-        // return;
-        
-        // Place remaining elements in tail, in ascending order
-        sort(nums.begin() + (i_break+1), nums.end()); 
+  void nextPermutation(vector<int> &nums) {
+    // From the back, move pointer till we find a drop, i.e. a[i] < a[i+1]. If
+    // no drop is found, then sort array and return. Here we have implemented a
+    // search for the last rise from the front.
+    int n = nums.size();
+    int i_break = -1;
+    for (int i = 0; i < (n - 1); i++)
+      if (nums[i] < nums[i + 1])
+        i_break = i;
+
+    if (i_break == -1) { // Fully descending from front
+      sort(nums.begin(), nums.end());
+      return;
     }
+
+    // For the remaining elements, find x (smallest element greater than a[i])
+    int x = INT_MAX;
+    int x_pos = -1;
+
+    for (int i = i_break + 1; i < n; i++)
+      if ((nums[i] > nums[i_break]) && (nums[i] < x)) {
+        x = nums[i];
+        x_pos = i;
+      }
+
+    // Swap nums[i] with x
+    swap(nums[i_break], nums[x_pos]);
+
+    // Place remaining elements in tail, in ascending order
+    sort(nums.begin() + (i_break + 1), nums.end());
+  }
 };
