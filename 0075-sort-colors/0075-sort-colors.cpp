b7890f042f106213208e5eb36edf31410d7b52c8
Joel Tony <joeltony72@gmail.com>
2024-05-30 17:53:18 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (71.50%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> freq(3, 0);
+        
+        for (int num: nums) freq[num]++;
+        
+        // for (auto x: freq) cout << x << " ";
+        // cout << endl;
+        
+        // 0 will be [0, freq[0])
+        // 1 will be [freq[0], freq[0] + freq[1])
+        // 2 will be [freq[0] + freq[1], freq[0] + freq[1] + freq[2])
+        for (int i = 0; i < freq[0]; i++) nums[i] = 0;
+        for (int i = freq[0]; i < (freq[0] + freq[1]); i++) nums[i] = 1;
+        for (int i = (freq[0] + freq[1]); i < (freq[0] + freq[1] + freq[2]); i++) nums[i] = 2;
+    }
+};
