6fbbcead8c9c0baccee6e3c70218a232b8c97f6a
Joel Tony <joeltony72@gmail.com>
2024-05-31 22:45:35 +0530

    Time: 78 ms (61.61%), Space: 63.5 MB (75.59%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size() - 1;
+        
+        // Set the value as negative if the number at that index is already seen
+        for (int i = 0; i < nums.size(); i++) {
+            int x = abs(nums[i]);
+            
+            if (nums[x-1] < 0)
+                return x;
+            else
+                nums[x-1] *= -1;
+        }
+        
+        return -1;
+    }
+};
