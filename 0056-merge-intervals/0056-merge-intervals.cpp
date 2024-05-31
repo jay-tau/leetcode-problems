61f02e36b412cbb4f33b84763e0bd1857d8f2c11
Joel Tony <joeltony72@gmail.com>
2024-05-31 21:27:34 +0530

    Time: 26 ms (40.41%), Space: 22.6 MB (68.49%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,22 +3,25 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
         
+        vector<vector<int>> ans;
+        
         sort(intervals.begin(), intervals.end());
         
-        vector<vector<int>> ans;
         
         vector<int> previous_interval = intervals[0];
         for (int i = 1; i < n; i++) {
             vector<int>& current_interval = intervals[i];
+            
             if (current_interval[0] <= previous_interval[1])
                 previous_interval[1] = max(previous_interval[1], current_interval[1]);
-            else {
+            
+            else { // Push into answer before starting new interval
                 ans.push_back(previous_interval);
                 previous_interval = current_interval;
             }
         }
         
-        ans.push_back(previous_interval); // Last interval to be pushed
+        ans.push_back(previous_interval); // Last interval is pushed
             
         return ans;
     }
