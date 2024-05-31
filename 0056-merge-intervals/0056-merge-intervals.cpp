642d3a266c85c2b55ac7084723ffb2466e3109b6
Joel Tony <joeltony72@gmail.com>
2024-05-31 21:27:46 +0530

    Time: 27 ms (33.59%), Space: 22.6 MB (68.49%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,27 @@
 class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        
-        vector<vector<int>> ans;
-        
-        sort(intervals.begin(), intervals.end());
-        
-        
-        vector<int> previous_interval = intervals[0];
-        for (int i = 1; i < n; i++) {
-            vector<int>& current_interval = intervals[i];
-            
-            if (current_interval[0] <= previous_interval[1])
-                previous_interval[1] = max(previous_interval[1], current_interval[1]);
-            
-            else { // Push into answer before starting new interval
-                ans.push_back(previous_interval);
-                previous_interval = current_interval;
-            }
-        }
-        
-        ans.push_back(previous_interval); // Last interval is pushed
-            
-        return ans;
+ public:
+  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    int n = intervals.size();
+
+    vector<vector<int>> ans;
+
+    sort(intervals.begin(), intervals.end());
+
+    vector<int> previous_interval = intervals[0];
+    for (int i = 1; i < n; i++) {
+      vector<int>& current_interval = intervals[i];
+
+      if (current_interval[0] <= previous_interval[1])
+        previous_interval[1] = max(previous_interval[1], current_interval[1]);
+
+      else {  // Push into answer before starting new interval
+        ans.push_back(previous_interval);
+        previous_interval = current_interval;
+      }
     }
+
+    ans.push_back(previous_interval);  // Last interval is pushed
+
+    return ans;
+  }
 };
