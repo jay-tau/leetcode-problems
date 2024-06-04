5d309176699a132d0be740f7eca2a300cceb524a
Joel Tony <joeltony72@gmail.com>
2024-06-04 11:44:36 +0530

    Time: 14 ms (81.96%), Space: 26.8 MB (47.68%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        // Pick an element. Score +1 for same, -1 for different.
+        int score = 0, x = -1;
+        for (int i = 0; i < n; i++) {
+            if (score <= 0) {
+                x = nums[i];
+                score = 0;
+            }
+            
+            if (nums[i] == x)
+                score++;
+            else
+                score--;
+        }
+        
+        printf("x: {%d}", x);
+        // Check if greater than n/2 after pick
+        
+        return x;
+    }
+};
